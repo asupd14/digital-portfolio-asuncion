971f75c564f1103ecd3c5fca3027af5434eb2150
asupena <asuncion498@hotmail.com>
2026-03-13 13:23:53 -0600

    Fix the routes to vercel

--- my-project/vite.config.ts
+++ my-project/vite.config.ts
@@ -2,7 +2,21 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        project: './the-project.html'
+      }
+    }
+  },
+  server: {
+    fs: {
+      allow: ['..']
+    }
+  }
 })
